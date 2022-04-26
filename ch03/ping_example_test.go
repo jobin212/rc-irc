ecc855f9a846fe3b588ef457d1f59d7231f5c00e
jobin212 <jobin212@gmail.com>
2022-04-26 15:11:59 -0700

    add ping

---
+++ ch03/ping_example_test.go
@@ -0,0 +1,68 @@
+package ch03
+
+import (
+	"context"
+	"io"
+	"log"
+	"time"
+)
+
+func ExamplePinger() {
+	ctx, cancel := context.WithCancel(context.Background())
+	r, w := io.Pipe() // in lieu of net.Conn
+	done := make(chan struct{})
+	resetTimer := make(chan time.Duration, 1)
+	resetTimer <- time.Second // initial ping interval
+
+	go func() {
+		Pinger(ctx, w, resetTimer)
+		close(done)
+	}()
+
+	log.Println("RUNNING")
+	receivePing := func(d time.Duration, r io.Reader) {
+		if d >= 0 {
+			log.Printf("resetting timer (%s)\n", d)
+			resetTimer <- d
+		}
+
+		now := time.Now()
+		buf := make([]byte, 1024)
+		n, err := r.Read(buf)
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		log.Printf("received %q (%s)\n",
+			buf[:n], time.Since(now).Round(100*time.Millisecond))
+	}
+
+	for i, v := range []int64{0, 200, 300, 0, -1, -1, -1} {
+		log.Printf("Run %d:\n", i+1)
+		receivePing(time.Duration(v)*time.Millisecond, r)
+	}
+
+	cancel()
+	<-done // ensures pinger exists after canceling context
+
+	// Output:
+	// Run 1:
+	// resetting timer (0s)
+	// received "ping" (1s)
+	// Run 2:
+	// resetting timer (200ms)
+	// received "ping" (200ms)
+	// Run 3:
+	// resetting timer (300ms)
+	// received "ping" (300ms)
+	// Run 4:
+	// resetting timer (0s)
+	// received "ping" (300ms)
+	// Run 5:
+	// received "ping" (300ms)
+	// Run 6:
+	// received "ping" (300ms)
+	// Run 7:
+	// received "ping" (300ms)
+}
