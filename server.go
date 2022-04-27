df205a9755ffc94cefef4137cf723492b2f92a9d
jobin <jobin212@gmail.com>
2022-04-26 23:52:48 -0700

    refactor (#5)

--- server.go
+++ server.go
@@ -55,77 +55,37 @@ func handleConnection(ic *IRCConn) {
 		ic.Conn.Close()
 	}()
 
-	client_address := ic.Conn.RemoteAddr().String()
-
 	scanner := bufio.NewScanner(ic.Conn)
 	scanner.Split(bufio.ScanLines)
 
 	for scanner.Scan() {
 		incoming_message := scanner.Text()
 		log.Printf("Incoming message: %s", incoming_message)
-		split_message := strings.Fields(incoming_message)
+		split_message := strings.SplitN(incoming_message, " ", 2)
 
 		if len(split_message) == 0 {
 			continue
 		}
 
-		var prefix string
+		var prefix string = ""
 		if strings.HasPrefix(split_message[0], ":") {
 			prefix = split_message[0]
 			log.Printf("Prefix %s\n", prefix)
-			split_message = split_message[1:]
+			split_message = strings.SplitN(split_message[1], " ", 2)
 		}
 
 		command := split_message[0]
-		params := split_message[1:]
+
+		var params string = ""
+		if len(split_message) >= 2 {
+			params = split_message[1]
+		}
 
 		switch command {
 		case "NICK":
-			nick := params[0]
-			log.Printf("NICK for connection %s is %s\n", client_address, nick)
-
-			_, ok := nickInUse[nick]
-			if nick != ic.Nick && ok {
-				msg := fmt.Sprintf(":%s 433 * %s :Nickname is already in use.\r\n",
-					ic.Conn.LocalAddr(), nick)
-				log.Printf(msg)
-				_, err := ic.Conn.Write([]byte(msg))
-				if err != nil {
-					log.Fatal(err)
-				}
-				break
-			}
-
-			nickInUse[nick] = true
-			ic.Nick = nick
-			if !ic.Welcomed && ic.User != "" {
-				msg := fmt.Sprintf(
-					":%s 001 %s :Welcome to the Internet Relay Network %s!%s@%s\r\n",
-					ic.Conn.LocalAddr(), ic.Nick, ic.Nick, ic.User, client_address)
-
-				log.Printf(msg)
-				_, err := ic.Conn.Write([]byte(msg))
-				if err != nil {
-					log.Fatal(err)
-				}
-				ic.Welcomed = true
-			}
+			handleNick(ic, params)
 		case "USER":
-			username := params[0]
-			ic.User = username
-
-			if !ic.Welcomed && ic.Nick != "" {
-				msg := fmt.Sprintf(
-					":%s 001 %s :Welcome to the Internet Relay Network %s!%s@%s\r\n",
-					ic.Conn.LocalAddr(), ic.Nick, ic.Nick, ic.User, client_address)
-
-				log.Printf(msg)
-				_, err := ic.Conn.Write([]byte(msg))
-				if err != nil {
-					log.Fatal(err)
-				}
-				ic.Welcomed = true
-			}
+			handleUser(ic, params)
 		default:
 			log.Println("Command not recognized")
 		}
@@ -135,4 +95,70 @@ func handleConnection(ic *IRCConn) {
 	if err != nil {
 		log.Printf("ERR: %v\n", err)
 	}
+}
+
+func handleNick(ic *IRCConn, params string) {
+	if !validateParameters("NICK", params, 1, ic) {
+		return
+	}
+
+	nick := strings.SplitN(params, " ", 2)[0]
+
+	_, ok := nickInUse[nick]
+	if nick != ic.Nick && ok {
+		msg := fmt.Sprintf(":%s 433 * %s :Nickname is already in use.\r\n",
+			ic.Conn.LocalAddr(), nick)
+		_, err := ic.Conn.Write([]byte(msg))
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	nickInUse[nick] = true
+	ic.Nick = nick
+
+	checkAndSendWelcome(ic)
+}
+
+func handleUser(ic *IRCConn, params string) {
+	if !validateParameters("USER", params, 4, ic) {
+		return
+	}
+
+	ic.User = strings.SplitN(params, " ", 2)[0]
+
+	checkAndSendWelcome(ic)
+}
+
+func checkAndSendWelcome(ic *IRCConn) {
+	if !ic.Welcomed && ic.Nick != "" && ic.User != "" {
+		msg := fmt.Sprintf(
+			":%s 001 %s :Welcome to the Internet Relay Network %s!%s@%s\r\n",
+			ic.Conn.LocalAddr(), ic.Nick, ic.Nick, ic.User, ic.Conn.RemoteAddr().String())
+
+		log.Printf(msg)
+		_, err := ic.Conn.Write([]byte(msg))
+		if err != nil {
+			log.Fatal(err)
+		}
+		ic.Welcomed = true
+	}
+}
+
+func validateParameters(command, params string, expectedNumParams int, ic *IRCConn) bool {
+	paramVals := strings.Fields(params)
+	if len(paramVals) < expectedNumParams {
+		msg := fmt.Sprintf(
+			":%s 461 %s :Not enough parameters\r\n",
+			ic.Conn.LocalAddr(), command)
+
+		log.Printf(msg)
+		_, err := ic.Conn.Write([]byte(msg))
+		if err != nil {
+			log.Fatal(err)
+		}
+		return false
+	}
+	return true
 }
